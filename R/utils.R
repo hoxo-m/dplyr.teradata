6a6024fc76e94f2c83d777168413bb4f9a30a7b5
hoxo-m <hoxo.smile@gmail.com>
2017-11-08 00:23:21 +0900

    fix

---
+++ R/utils.R
@@ -0,0 +1,15 @@
+#' @export
+db_list_tables_with_pattern <- function(con, pattern) {
+  table_names <- db_list_tables(con)
+  Filter(function(x) grepl(pattern, x), table_names)
+}
+
+#' @export
+raw_to_string <- function(raw) {
+  vapply(raw, function(x) paste(as.character(x), collapse = ""), character(1))
+}
+
+dot_to_underscore <- function(x) {
+  gsub(".", "_", x, fixed = TRUE)
+}
+
