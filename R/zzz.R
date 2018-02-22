4d638f4fb9612791b81e91fd6369de8e26737767
Koji Makiyama <hoxo.smile@gmail.com>
2018-02-22 20:41:35 +0900

    fix

--- R/zzz.R
+++ R/zzz.R
@@ -11,6 +11,7 @@
   assign("cut", sql_cut, envir = dbplyr::base_odbc_scalar)
   assign("like", sql_like, envir = dbplyr::base_odbc_scalar)
   assign("to_timestamp", sql_to_timestamp, envir = dbplyr::base_odbc_scalar)
+  assign("%%", sql_mod, envir = dbplyr::base_odbc_scalar)
   assign("count_if", sql_count_if, envir = dbplyr::base_odbc_scalar)
   assign("n_if", sql_count_if, envir = dbplyr::base_odbc_scalar)
 
