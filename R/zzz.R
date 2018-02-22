27b65f217bb2c95682d565f2abc697a31d596f79
Koji Makiyama <hoxo.smile@gmail.com>
2018-02-22 19:51:03 +0900

    add bool_to_int()

--- R/zzz.R
+++ R/zzz.R
@@ -13,6 +13,7 @@
   assign("to_timestamp", sql_to_timestamp, envir = dbplyr::base_odbc_scalar)
   assign("count_if", sql_count_if, envir = dbplyr::base_odbc_scalar)
   assign("n_if", sql_count_if, envir = dbplyr::base_odbc_scalar)
+  assign("bool_to_int", sql_bool_to_int, envir = dbplyr::base_odbc_scalar)
 
   # Attaching packages ------------------------------------------------------
   # See https://github.com/tidyverse/tidyverse/blob/master/R/zzz.R
